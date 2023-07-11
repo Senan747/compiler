c0bcee55b76184a1ef7c4bc32a6b4710be3408fe
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-07-11 23:05:29 +0400

    base styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const MyComponent = () => {
   const [language, setLanguage] = useState('');
   const [input, setInput] = useState('');
   const [result, setResult] = useState('');
+  const [rows, setRows] = useState('25')
 
   const fetchData = async () => {
     const code = encodeURIComponent(input);
@@ -43,21 +44,51 @@ const MyComponent = () => {
     }
   };
 
+  const renderList = () => {
+    const listItems = [];
+    for (let i = 1; i < 25; i++) {
+      listItems.push(<li key={i}>{i}</li>);
+    }
+    return listItems;
+  };
+
   return (
-    <div>
-      <select value={language} onChange={handleSelection}>
-        <option value="">Select a language</option>
-        <option value="py">Python</option>
-        <option value="java">Java</option>
-        <option value="cpp">C++</option>
-        <option value="c">C</option>
-        <option value="go">GoLang</option>
-        <option value="cs">C#</option>
-        <option value="js">NodeJS</option>
-      </select>
+    <div className='flex flex-row h-screen w-screen bg-gega-dark'>
+
+      {/* 
       <input type="text" value={input} onChange={handleInput} />
       <button onClick={handleRun}>Run</button>
-      <div>Result: {result}</div>
+      <div>Result: {result}</div> */}
+      <div className='m-9 border-2 border-gega-green min-w-[700px]'>
+        <div className=''>
+          <select value={language} onChange={handleSelection}>
+            <option value="">Select a language</option>
+            <option value="py">Python</option>
+            <option value="java">Java</option>
+            <option value="cpp">C++</option>
+            <option value="c">C</option>
+            <option value="go">GoLang</option>
+            <option value="cs">C#</option>
+            <option value="js">NodeJS</option>
+          </select>
+          <button className='border-none bg-gega-green text-black' onClick={handleRun}>Run</button>
+        </div>
+        <div className='flex flex-row'>
+          <div className='flex flex-col min-w-[20px] text-gega-white'>
+            {
+              <ul>{renderList()}</ul>
+            }
+    
+          </div>
+          <div>
+            <textarea name="input" id="" cols="85" rows={rows}></textarea>
+          </div>
+           
+        </div>
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,74 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px',
+        md: '900px'
+      }
+    },
+    extend: {
+      keyframes: {
+        pulse: {
+          '0%': { opacity: 0 },
+          '50%': { opacity: 0.5 },
+          '100%': { opacity: 1},   
+        },
+        wiggle: {
+          '0%, 100%': { transform: 'rotaate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        new: {
+          from: { top: '384px'},
+          to: { top: '0px'}
+        },
+        fadeIn: {
+          from: {
+            opacity: 0,
+            transform: 'translateY(-10px)',
+          },
+          to: {
+            opacity: 1,
+            transform: 'translateY(0)'
+          }
+        }
+      },
+      fontFamily: {
+        Roboto: ['Roboto', 'sans-serif']
+      },
+      backgroundImage: {
+        'quiz-project': "url('../src/assets/quiz-project.jpeg')",
+        'editor-project': "url('../src/assets/editor-project.jpeg')",
+        'travel-project': "url('../src/assets/travel-project.jpeg')",
+        'web-search-project': "url('../src/assets/websearch.jpg')"
+      },
+      borderRadius: {
+        '75': '75%',
+        '50': '50%',
+        '30': '30%'
+      },
+      borderWidth: {
+        '1': '1px',
+        '0.5': '0.5px',
+      },
+      colors: {
+        'gega-red': '#bc1a45',
+        'gega-melon': '#ffd369',
+        'gega-grey': '#dddddd',
+        'gega-white': '#f7f7f7',
+        'gega-black': '#1F1F1F',
+        'gega-light': '#999999',
+        'gega-green': '#28E98C',
+        'gega-dark': '#191919',
+      },
+    },
+  },
+  plugins: [],
+};
