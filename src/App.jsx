d17e0d5c4952e8ab600a1ef6067645e4b66e9938
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-07-13 07:45:51 +0400

    suggest keywords

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ const MyComponent = () => {
   const [rows, setRows] = useState('25')
   const [error, setError] = useState("")
   const [userInput, setUserInput] = useState("")
+// const [code, setCode] = useState('');
+  const [suggestedKeywords, setSuggestedKeywords] = useState([]);
+  const [selectedKeyword, setSelectedKeyword] = useState('');
 
   const fetchData = async () => {
     const code = encodeURIComponent(input);
@@ -41,6 +44,28 @@ const MyComponent = () => {
 
   const handleInput = (e) => {
     setInput(e.target.value);
+    const inputCode = event.target.value;
+  setInput(inputCode);
+
+  // Kod analizi
+  const lines = inputCode.split('\n');
+  const lastLine = lines[lines.length - 1];
+  const words = lastLine.trim().split(' ');
+  const lastWord = words[words.length - 1];
+
+
+  const cppKeywords = [
+    'auto', 'break', 'case', 'char', 'class', 'const', 'continue', 'default',
+    'delete', 'do', 'double', 'else', 'enum', 'false', 'float', 'for',
+    'if', 'int', 'long', 'new', 'null', 'operator', 'private', 'protected',
+    'public', 'return', 'short', 'sizeof', 'static', 'struct', 'switch',
+    'template', 'this', 'true', 'typedef', 'union', 'unsigned', 'void',
+    'while'
+    ];
+
+  // Anahtar kelime önerisi
+  const suggested = cppKeywords.filter(keyword => keyword.startsWith(lastWord));
+  setSuggestedKeywords(suggested);
   };
 
   const handleRun = () => {
@@ -64,6 +89,41 @@ const MyComponent = () => {
     }
     return listItems;
   };
+const handleCodeChange = (event) => {
+  const inputCode = event.target.value;
+  setInput(inputCode);
+
+  // Kod analizi
+  const lines = inputCode.split('\n');
+  const lastLine = lines[lines.length - 1];
+  const words = lastLine.trim().split(' ');
+  const lastWord = words[words.length - 1];
+
+
+  const cppKeywords = [
+    'auto', 'break', 'case', 'char', 'class', 'const', 'continue', 'default',
+    'delete', 'do', 'double', 'else', 'enum', 'false', 'float', 'for',
+    'if', 'int', 'long', 'new', 'null', 'operator', 'private', 'protected',
+    'public', 'return', 'short', 'sizeof', 'static', 'struct', 'switch',
+    'template', 'this', 'true', 'typedef', 'union', 'unsigned', 'void',
+    'while'
+    ];
+
+  // Anahtar kelime önerisi
+  const suggested = cppKeywords.filter(keyword => keyword.startsWith(lastWord));
+  setSuggestedKeywords(suggested);
+};
+
+const handleSuggestionClick = (keyword) => {
+  setSelectedKeyword(keyword);
+  const lines = code.split('\n');
+  const lastLine = lines[lines.length - 1];
+  const words = lastLine.trim().split(' ');
+  words[words.length - 1] = keyword;
+  lines[lines.length - 1] = words.join(' ');
+  const updatedCode = lines.join('\n');
+  setInput(updatedCode);
+};
 
   return (
     <div className='flex flex-row h-screen w-screen bg-gega-dark'>
@@ -88,8 +148,13 @@ const MyComponent = () => {
             }
           </div>
           <div className='bg-gega-light-black'>
-            <textarea value={input} onChange={handleInput} className='bg-gega-light-black text-gega-white pl-1' name="input" id="" cols="85" rows={rows}></textarea>
+            <textarea value={input} onChange={handleInput}  className='bg-gega-light-black text-gega-white pl-1' name="input" id="" cols="85" rows={rows}></textarea>
           </div>
+            <ul style={{ position: 'absolute', top: '100%', left: 0, zIndex: 1, background: 'black', color: 'white' }}>
+            {suggestedKeywords.map((keyword, index) => (
+              <li key={index} onClick={() => handleSuggestionClick(keyword)}>{keyword}</li>
+            ))}
+            </ul>
         </div>
       </div>
       <div className={`flex flex-col min-h-[700px] min-w-[700px] m-9 border-2 border-gega-green ${error ? 'border-red-700' : 'border-gega-green'}`}>
@@ -107,3 +172,5 @@ const MyComponent = () => {
 };
 
 export default MyComponent;
+
+
